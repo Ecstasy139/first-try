8500b80e48eb3ba66776bb59a580505dcb0bfc67
Ecstasy139 <459506879@qq.com>
2022-03-24 16:32:08 +0800

    Update tensorboard

--- train_res.py
+++ train_res.py
@@ -4,7 +4,6 @@ import torch
 import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
 from model_res import *
 from dataloader2 import *
 
@@ -18,9 +17,9 @@ def train():
     epochs = 40
     batch_size = 10
     learning_rate = 0.01
-
+    total_loss = 0
     writer = SummaryWriter('logs_train')
-    weight_path = 'params/net_res.pth'
+    weight_path = 'params/net_res1.pth'
     device = torch.device('cuda')
     net = ResNet().to(device)
     if os.path.exists(weight_path):  # Load weight file
@@ -39,6 +38,7 @@ def train():
 
     for epoch in range(epochs):
         net.train()
+        total_loss = 0
         for i, (img, label) in enumerate(dataloader):
             img, label = img.to(device), label.to(device)
             #print(img.shape, label)
@@ -47,14 +47,16 @@ def train():
             loss = loss_fn(output, label)
             # print(output.dtype, loss.dtype)
             print('{}-{}-Loss:{}'.format(epoch, i, loss))
-            writer.add_scalar('train_loss', loss.item(), i)
+            total_loss = total_loss + loss
 
             optim.zero_grad()
             loss.backward()
             optim.step()
 
+        writer.add_scalar('train_loss', total_loss.item(), epoch)
+
         if epoch % 10 == 0:
-            torch.save(net.state_dict(), f'params/net_res_visual.pth')
+            torch.save(net.state_dict(), f'params/net_res1.pth')
 
     writer.close()
 
